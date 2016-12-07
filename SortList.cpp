4cf3fac3b79b4f9ef6614ea0f4257b9629cde040
Skiini <skiini03@gmail.com>
2016-12-07 14:09:04 +0100

    Adding the function's bubble, insert and Quick. Need to check how to use access element in a list

--- SortList.cpp
+++ SortList.cpp
@@ -16,10 +16,90 @@ void show_list(std::list<int>& intList)
 	}
 }
 
+void bubble_sort_list(std::list<int>& intList)
+{
+	int i, j, flag = 1;    // set flag to 1 to start first pass
+	int temp;             // holding variable
+	int numLength = intList.size();
+	for (i = 1; (i <= numLength) && flag; i++)
+	{
+		flag = 0;
+		for (j = 0; j < (numLength - 1); j++)
+		{
+			if (intList[j + 1] > intList[j])      // ascending order simply changes to <
+			{
+				temp = intList[j];             // swap elements
+				intList[j] = intList[j + 1];
+				intList[j + 1] = temp;
+				flag = 1;               // indicates that a swap occurred.
+			}
+		}
+	}
+	return;
+}
+
+void insertion_sort(std::list<int>& intList) {
+	int j, temp;
+	int length = intList.size();
+	for (int i = 0; i < length; i++) {
+		j = i;
+
+		while (j > 0 && intList[j] < intList[j - 1]) {
+			temp = intList[j];
+			intList[j] = intList[j - 1];
+			intList[j - 1] = temp;
+			j--;
+		}
+	}
+}
+
+void swap(int i, int j, std::list<int>& intList)
+{
+	int temp = a[i];
+	a[i] = a[j];
+	a[j] = temp;
+}
+
+
+void quicksort(std::list<int>& intList) 
+{
+	
+	int left = intList.begin();
+	int right = intList.end();
+	int min = (left + right) / 2;
+
+	int i = left;
+	int j = right;
+	int pivot = intList[min];
+
+	while (left<j || i<right)
+	{
+		while (intList[i]<pivot)
+			i++;
+		while (intList[j]>pivot)
+			j--;
+
+		if (i <= j) {
+			swap(i, j, intList);
+			i++;
+			j--;
+		}
+		else {
+			if (left<j)
+				quicksort(intList, left, j);
+			if (i<right)
+				quicksort(intList, i, right);
+			return;
+		}
+	}
+}
+
 
 void sort_list(std::list<int>& intList)
 {
-	//TODO sort the list from the smallest to the biggest number
+	//TODO sort the list from the smallest to the biggest number,
+	//Mayby call the good functions (Bubble, insertion and quick)
+
 }
 
 
